749f60b7b05eddd36b6446ff5b17e20a6b686d09
cuongcmnr <cuongidf987@outlook.com>
2024-12-31 14:18:57 +0700

    Update check.js

--- docs/check.js
+++ docs/check.js
@@ -2,7 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const loggedInUser = localStorage.getItem('loggedInUser');
     const actionsDiv = document.querySelector('.actions');
     if (loggedInUser) {
-        actionsDiv.innerHTML = `<span>Welcome, ${loggedInUser}</span> <button class="logout" onclick="logout()">Logout</button>`;
+        actionsDiv.innerHTML = `
+        <button class="review" onclick="location.href='select-place.html'">Write a Review</button>
+        <button class="plan" onclick="location.href='plan-trip.html'">Plan Your Trip</button>
+        <span>Welcome, ${loggedInUser}</span> <button class="logout" onclick="logout()">Logout</button>`;
     }
 
     const header = document.querySelector('.custom-header');
